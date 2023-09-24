f1ae8cf8a9125e9e6eac87e6b759401cb0e7e993
Krishna <krishna13248@gmail.com>
2023-09-24 20:24:32 +0530

    first

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "../styles/globals.css";
+import type { Metadata } from "next";
+import { Inter, Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Lofi Toss",
+  description: "Build by Krishna",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
